8e07549fdea6d9f2c2b0e58507b1eca2cbdfe873
Endres <github@e7p.de>
2014-09-26 09:46:14 +0200

    unified spacing

--- draw.py
+++ draw.py
@@ -1,60 +1,60 @@
 # copied from http://www.roguebasin.com/index.php?title=Bresenham%27s_Line_Algorithm#Python
 def get_line(x1, y1, x2, y2):
-    points = []
-    issteep = abs(y2-y1) > abs(x2-x1)
-    if issteep:
-        x1, y1 = y1, x1
-        x2, y2 = y2, x2
-    rev = False
-    if x1 > x2:
-        x1, x2 = x2, x1
-        y1, y2 = y2, y1
-        rev = True
-    deltax = x2 - x1
-    deltay = abs(y2-y1)
-    error = int(deltax / 2)
-    y = y1
-    ystep = None
-    if y1 < y2:
-        ystep = 1
-    else:
-        ystep = -1
-    for x in range(x1, x2 + 1):
-        if issteep:
-            points.append((y, x))
-        else:
-            points.append((x, y))
-        error -= deltay
-        if error < 0:
-            y += ystep
-            error += deltax
-    # Reverse the list if the coordinates were reversed
-    if rev:
-        points.reverse()
-    return points
+	points = []
+	issteep = abs(y2-y1) > abs(x2-x1)
+	if issteep:
+		x1, y1 = y1, x1
+		x2, y2 = y2, x2
+	rev = False
+	if x1 > x2:
+		x1, x2 = x2, x1
+		y1, y2 = y2, y1
+		rev = True
+	deltax = x2 - x1
+	deltay = abs(y2-y1)
+	error = int(deltax / 2)
+	y = y1
+	ystep = None
+	if y1 < y2:
+		ystep = 1
+	else:
+		ystep = -1
+	for x in range(x1, x2 + 1):
+		if issteep:
+			points.append((y, x))
+		else:
+			points.append((x, y))
+		error -= deltay
+		if error < 0:
+			y += ystep
+			error += deltax
+	# Reverse the list if the coordinates were reversed
+	if rev:
+		points.reverse()
+	return points
 
 def draw_dots(chars, dots):
-   # find out average of Y-coordinate, for all of these in the invisible middle line
-   bottom = top = 0
-   for dot in dots:
-      if dot[1] > 8:
-        bottom += 1
-      if dot[1] < 8:
-        top += 1
-   print "We have %d bottom pixels and %d top pixels" % (bottom, top)
+	# find out average of Y-coordinate, for all of these in the invisible middle line
+	bottom = top = 0
+	for dot in dots:
+		if dot[1] > 8:
+			bottom += 1
+		if dot[1] < 8:
+			top += 1
+	print "We have %d bottom pixels and %d top pixels" % (bottom, top)
 
-   for dot in dots:
-      if dot[1] == 8 and dot[0] != 8: # invisible line
-         dot = (dot[0], dot[1]-1 if bottom < top or (bottom == top and dot[0] < 8) else dot[1]+1) # bugfix suitable for clock (pointer is rotated around the center)
-      if dot[0] < 5 and dot[1] < 8:
-         chars[0][dot[1]] |= (1<<(4-dot[0]))
-      elif 5 < dot[0] < 11 and dot[1] < 8:
-         chars[1][dot[1]] |= (1<<(4-(dot[0]-6)))
-      elif 11 < dot[0] < 17 and dot[1] < 8:
-         chars[2][dot[1]] |= (1<<(4-(dot[0]-12)))
-      elif dot[0] < 5 and 8 < dot[1]:
-         chars[3][dot[1]-9] |= (1<<(4-dot[0]))
-      elif 5 < dot[0] < 11 and 8 < dot[1]:
-         chars[4][dot[1]-9] |= (1<<(4-(dot[0]-6)))
-      elif 10 < dot[0] < 17 and 8 < dot[1]:
-         chars[5][dot[1]-9] |= (1<<(4-(dot[0]-12)))
+	for dot in dots:
+		if dot[1] == 8 and dot[0] != 8: # invisible line
+			dot = (dot[0], dot[1]-1 if bottom < top or (bottom == top and dot[0] < 8) else dot[1]+1) # bugfix suitable for clock (pointer is rotated around the center)
+		if dot[0] < 5 and dot[1] < 8:
+			chars[0][dot[1]] |= (1<<(4-dot[0]))
+		elif 5 < dot[0] < 11 and dot[1] < 8:
+			chars[1][dot[1]] |= (1<<(4-(dot[0]-6)))
+		elif 11 < dot[0] < 17 and dot[1] < 8:
+			chars[2][dot[1]] |= (1<<(4-(dot[0]-12)))
+		elif dot[0] < 5 and 8 < dot[1]:
+			chars[3][dot[1]-9] |= (1<<(4-dot[0]))
+		elif 5 < dot[0] < 11 and 8 < dot[1]:
+			chars[4][dot[1]-9] |= (1<<(4-(dot[0]-6)))
+		elif 10 < dot[0] < 17 and 8 < dot[1]:
+			chars[5][dot[1]-9] |= (1<<(4-(dot[0]-12)))
--- main.py
+++ main.py
@@ -11,11 +11,11 @@ import copy
 import math
 
 CIRCLE_CHARS = [[0x00, 0x01, 0x02, 0x04, 0x08, 0x08, 0x10, 0x10],
-       		    [0x1F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
-          		[0x00, 0x10, 0x08, 0x04, 0x02, 0x02, 0x01, 0x01],
-         		[0x10, 0x10, 0x08, 0x08, 0x04, 0x02, 0x01, 0x00],
-         		[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1F],
-        		[0x01, 0x01, 0x02, 0x02, 0x04, 0x08, 0x10, 0x00]]
+				[0x1F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+				[0x00, 0x10, 0x08, 0x04, 0x02, 0x02, 0x01, 0x01],
+				[0x10, 0x10, 0x08, 0x08, 0x04, 0x02, 0x01, 0x00],
+				[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1F],
+				[0x01, 0x01, 0x02, 0x02, 0x04, 0x08, 0x10, 0x00]]
 
 class main:
 	nointerrupt = True
@@ -26,20 +26,20 @@ class main:
 		# store next full minute
 		starttime = time.time() - time.time() % 60 + 60
 		while self.nointerrupt:
-	         t = datetime.datetime.now()
-	         clockcircle = copy.deepcopy(CIRCLE_CHARS)
-	         draw.draw_dots(clockcircle, draw.get_line(8, 8, \
-	          int(round(math.cos(((9 + t.hour + t.minute / 60.0) % 12) / 12.0 * 2 * math.pi + 0.00) * 4 + 8)), \
-	          int(round(math.sin(((9 + t.hour + t.minute / 60.0) % 12) / 12.0 * 2 * math.pi + 0.00) * 4 + 8))))
-	         draw.draw_dots(clockcircle, draw.get_line(8, 8, \
-	          int(round(math.cos(((45 + t.minute) % 60) / 60.0 * 2 * math.pi + 0.00) * 8 + 8)), \
-	          int(round(math.sin(((45 + t.minute) % 60) / 60.0 * 2 * math.pi + 0.00) * 8 + 8))))
-	         lcd_device.load_custom_chars(clockcircle)
-	         
-	         lcd_device.puts("\x00\x01\x02 "+t.strftime("%x"), 0)
-	         lcd_device.puts("\x03\x04\x05 "+t.strftime("  %H:%M"), 1)
-	         print "loop took %f ms" % ((time.time() - starttime) % 1.0 * 1000)
-	         time.sleep(60.0 - ((time.time() - starttime) % 60.0))
+			t = datetime.datetime.now()
+			clockcircle = copy.deepcopy(CIRCLE_CHARS)
+			draw.draw_dots(clockcircle, draw.get_line(8, 8, \
+				int(round(math.cos(((9 + t.hour + t.minute / 60.0) % 12) / 12.0 * 2 * math.pi + 0.00) * 4 + 8)), \
+				int(round(math.sin(((9 + t.hour + t.minute / 60.0) % 12) / 12.0 * 2 * math.pi + 0.00) * 4 + 8))))
+			draw.draw_dots(clockcircle, draw.get_line(8, 8, \
+				int(round(math.cos(((45 + t.minute) % 60) / 60.0 * 2 * math.pi + 0.00) * 8 + 8)), \
+				int(round(math.sin(((45 + t.minute) % 60) / 60.0 * 2 * math.pi + 0.00) * 8 + 8))))
+			lcd_device.load_custom_chars(clockcircle)
+
+			lcd_device.puts("\x00\x01\x02 "+t.strftime("%x"), 0)
+			lcd_device.puts("\x03\x04\x05 "+t.strftime("  %H:%M"), 1)
+			print "loop took %f ms" % ((time.time() - starttime) % 1.0 * 1000)
+			time.sleep(60.0 - ((time.time() - starttime) % 60.0))
 
 	def __init__(self) :
 		global lcd_device
@@ -50,4 +50,4 @@ class main:
 		self.analog_clock()
 
 if __name__ == '__main__':
-    main()
+	main()
